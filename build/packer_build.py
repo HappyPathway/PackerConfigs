274e7ce39099728826eef40c4cc8775d061f8903
Dave Arnold <dave@alphonso.tv>
2018-03-15 14:14:23 -0700

    working on chef public ami

--- build/packer_build.py
+++ build/packer_build.py
@@ -23,7 +23,7 @@ def main(opt):
                                 stderr=subprocess.PIPE)
             out, err = p.communicate()
             if p.returncode > 0:
-                errors.append(dict(file=changed_file, reason=err, msg=out))
+                errors.append(dict(file=changed_file, reason=err))
         except Exception, e:
             if changed_file.endswith(".json"):
                 errors.append(dict(file=changed_file, reason=str(e)))
